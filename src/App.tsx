88446a3e19975348e25568bf06fd9c87f718ba3d
DebjeetDam <debjeet.dam95@gmail.com>
2024-04-24 21:37:13 -0500

    pass children

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
+import Alert from "./components/Alert";
+
 // import Message from './Message'
-import ListGroup from "./components/ListGroup";
-
 function App() {
-  let items = ["New York", "SFC", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        onSelectItem={handleSelectItem}
-        heading="Hello Cities"
-      />
+      <Alert>Helloo Workd</Alert>
     </div>
   );
 }
