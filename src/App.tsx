c12d85a5feee861ffd1a8228da8e50bd164acc57
DebjeetDam <debjeet.dam95@gmail.com>
2024-04-24 21:21:27 -0500

    ListGroup props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 // import Message from './Message'
 import ListGroup from "./components/ListGroup";
 
-const items = ["New York", "SFC", "Tokyo", "London", "Paris"];
-
 function App() {
+  let items = ["New York", "SFC", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        onSelectItem={handleSelectItem}
+        heading="Hello Cities"
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,19 +2,25 @@
 
 import { useState } from "react";
 
-// {items : [], heading: string}
+// Define interface of an object
 interface Props {
+  // {items : [], heading: string}
   items: string[];
   heading: string;
+  // (item:string) => void
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
-  // Hook
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  //destructuring props {}
+  // HOOK
+  // useState->const [variable(selectedIndex), updater func] = useState('')
   const [selectedIndex, setSelectedIndex] = useState(-1);
-  //   const [name, setName] = useState('')
+
   return (
     <>
       <h1>{heading}</h1>
+      {/* conditional rendering w/logical AND */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
