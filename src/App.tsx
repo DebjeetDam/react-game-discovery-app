322034f2197beae966dc7357a4afe58fc7820c48
DebjeetDam <debjeet.dam95@gmail.com>
2024-04-25 21:46:46 -0500

    Expendabld text exercise

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 import Like from "./components/Like";
+import ExpandableText from "./components/ExpandableText";
 
 // import Message from './Message'
 function App() {
@@ -10,6 +11,7 @@ function App() {
   const [alertVisible, setAlertVisible] = useState(false);
   return (
     <div>
+      <ExpandableText maxChars={100}>Hello World</ExpandableText>
       <Like
         onClick={() => {
           console.log("clicked");
@@ -27,3 +29,46 @@ function App() {
 }
 
 export default App;
+
+// //Exercise 1
+// const [game, setGame] = useState({
+//   id: 1,
+//   player: {
+//     name: "Deb",
+//   },
+// });
+// const handleClick = () => {
+//   setGame({
+//     ...game,
+//     player: {
+//       name: "Son",
+//     },
+//   });
+// };
+// //Exercise 2
+// const [pizza, setPizza] = useState({
+//   name: "paneer pizza",
+//   toppings: ["Mushroom"],
+// });
+
+// const handlePizza = () => {
+//   setPizza({ ...pizza, toppings: [...pizza.toppings, "Peppers"] });
+// };
+
+// //Exercise 3
+// const [cart, setCart] = useState({
+//   discount: 0.1,
+//   items: [
+//     { id: 1, title: "P1", quantity: 1 },
+//     { id: 2, title: "P2", quantity: 1 },
+//   ],
+// });
+
+// const handleCart = () => {
+//   setCart({
+//     ...cart,
+//     items: cart.items.map((item) => {
+//       item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item;
+//     }),
+//   });
+// };
