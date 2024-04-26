28e621e6f25701e56bdd1c299bf724c4244008af
DebjeetDam <debjeet.dam95@gmail.com>
2024-04-26 18:06:57 -0500

    Validation using react hook form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,37 +1,52 @@
-import { FormEvent, useState } from "react";
+// import { FormEvent, useState } from "react";
+import { useForm, FieldValues } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
 
 const Form = () => {
   // useState HOOK to read input values
-  const [person, setPerson] = useState({ name: "", age: "" });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-    // stop page from reloading
-  };
+  console.log(errors);
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();    // stop page from reloading
+  //   console.log(person);
+  // };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          onChange={(e) => setPerson({ ...person, name: e.target.value })}
-          value={person.name}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">name of field reqd</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">inc. length</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Age
         </label>
         <input
-          onChange={(e) =>
-            setPerson({ ...person, age: parseInt(e.target.value) })
-          }
-          value={person.age}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
