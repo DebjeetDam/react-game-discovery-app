89ed73790e289cc98a404180642042b7a66c706f
DebjeetDam <debjeet.dam95@gmail.com>
2024-04-26 19:01:36 -0500

    Expense List component and events

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,61 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
-import Like from "./components/Like";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
+// import { useState } from "react";
+// import Alert from "./components/Alert";
+// import Button from "./components/Button";
+// import ListGroup from "./components/ListGroup";
+// import Like from "./components/Like";
+// import ExpandableText from "./components/ExpandableText";
+// import Form from "./components/Form";
 import "./index.css";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { useState } from "react";
 
 // import Message from './Message'
-function App() {
-  const items = ["NYC", "LA", "SFC"];
-  const [alertVisible, setAlertVisible] = useState(false);
+
+export const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 1, category: "Utility" },
+    { id: 2, description: "aaa", amount: 1, category: "Utility" },
+    { id: 3, description: "aaa", amount: 1, category: "Utility" },
+    { id: 4, description: "aaa", amount: 1, category: "Utility" },
+  ]);
+
   return (
     <div>
-      <Form />
-      <ExpandableText maxChars={100}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
-        alias error, fuga labore quibusdam sed quasi, ipsum cupiditate
-        consequuntur adipisci at dolore magnam. Consequuntur fugit, molestiae
-        omnis tempore recusandae nostrum!100
-      </ExpandableText>
-      <Like
-        onClick={() => {
-          console.log("clicked");
-        }}
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
       />
-      <ListGroup items={items} heading="Miami" onSelectItem={() => {}} />
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
-      )}
-      <Button color="warning" onClick={() => setAlertVisible(true)}>
-        Hello Button Test
-      </Button>
     </div>
   );
-}
+};
+
+// function App() {
+//   const items = ["NYC", "LA", "SFC"];
+//   const [alertVisible, setAlertVisible] = useState(false);
+//   return (
+//     <div>
+//       <Form />
+//       <ExpandableText maxChars={100}>
+//         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
+//         alias error, fuga labore quibusdam sed quasi, ipsum cupiditate
+//         consequuntur adipisci at dolore magnam. Consequuntur fugit, molestiae
+//         omnis tempore recusandae nostrum!100
+//       </ExpandableText>
+//       <Like
+//         onClick={() => {
+//           console.log("clicked");
+//         }}
+//       />
+//       <ListGroup items={items} heading="Miami" onSelectItem={() => {}} />
+//       {alertVisible && (
+//         <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
+//       )}
+//       <Button color="warning" onClick={() => setAlertVisible(true)}>
+//         Hello Button Test
+//       </Button>
+//     </div>
+//   );
+// }
 
 export default App;
 
