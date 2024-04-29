ba87c360f9bdc022ed54ac6e5cb76ecb8cee459a
DebjeetDam <debjeet.dam95@gmail.com>
2024-04-29 12:51:59 -0500

    Adding validation using Zod

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,7 @@ import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Utility", "Entertainment"];
+import categories from "./expense-tracker/categories";
 
 export const App = () => {
   //State for ExpenseFilter
