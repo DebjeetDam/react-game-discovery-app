f046cad22267b8d9158adf1810ad2b29b12dfe5a
DebjeetDam <debjeet.dam95@gmail.com>
2024-04-29 22:31:08 -0500

    get

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 function Data() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   interface User {
     id: number;
@@ -9,17 +10,31 @@ function Data() {
   }
 
   useEffect(() => {
+    const controller = new AbortController();
+
+    //get -> wait -> res /err
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
   }, []);
 
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      <p className="text-danger"> {error} </p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
