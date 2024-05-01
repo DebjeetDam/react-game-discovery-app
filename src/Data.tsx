16a8b279ede6c475ef83b1303e243995db21c3c8
DebjeetDam <debjeet.dam95@gmail.com>
2024-04-30 19:57:48 -0500

    changes in Data

--- src/Data.tsx
+++ src/Data.tsx
@@ -10,7 +10,7 @@ function Data() {
   useEffect(() => {
     //get -> wait -> res /err
     setLoading(true);
-    const { request, cancel } = userService.getAllUser();
+    const { request, cancel } = userService.getAll<User>();
     request
       .then((res) => {
         setUsers(res.data);
@@ -28,7 +28,7 @@ function Data() {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
 
-    userService.deleteUser(user.id).catch((err) => {
+    userService.delete(user.id).catch((err) => {
       setError(err.message);
       setUsers(originalUsers);
     });
@@ -40,7 +40,7 @@ function Data() {
     setUsers([newUser, ...users]);
 
     userService
-      .createUser(newUser)
+      .create(newUser)
       .then(({ data: savedUser }) => {
         setUsers([savedUser, ...users]);
       })
@@ -55,7 +55,7 @@ function Data() {
     const updatedUser = { ...user, name: user.name + "!@!" };
     setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
 
-    userService.updateUser(updatedUser).catch((err) => {
+    userService.update(updatedUser).catch((err) => {
       setError(err.message);
       setUsers(originalUsers);
     });
